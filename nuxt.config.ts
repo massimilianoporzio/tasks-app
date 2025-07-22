0be4d681d78ed165dd31b9da4b7a38735013f865
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-07-22 15:30:31 +0200

    pico css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,9 @@
 export default defineNuxtConfig({
 	modules: ["@nuxt/eslint"],
 	devtools: { enabled: true },
+	css: ["@picocss/pico"], // global css for all the pages
 	compatibilityDate: "2025-07-15",
+
 	eslint: {
 		config: {
 			stylistic: {
