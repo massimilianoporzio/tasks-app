3b470e557d18aafba9b60e73d0e35518e9c03b24
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-07-22 23:17:43 +0200

    nav bar come component

---
+++ app/components/app/AppNavBar.vue
@@ -0,0 +1,12 @@
+<template>
+	<nav class="container">
+		<ul>
+			<li><strong>Acme Corp</strong></li>
+		</ul>
+		<ul>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Services</a></li>
+			<li><a href="#">Products</a></li>
+		</ul>
+	</nav>
+</template>
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,15 +1,10 @@
+<script setup lang="ts">
+import AppNavBar from "~/components/app/AppNavBar.vue";
+</script>
+
 <template>
 	<div>
-		<nav class="container">
-			<ul>
-				<li><strong>Acme Corp</strong></li>
-			</ul>
-			<ul>
-				<li><a href="#">About</a></li>
-				<li><a href="#">Services</a></li>
-				<li><a href="#">Products</a></li>
-			</ul>
-		</nav>
+		<AppNavBar />
 		<main class="container">
 			<slot />
 		</main>
